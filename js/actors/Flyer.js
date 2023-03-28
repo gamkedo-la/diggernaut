c34f53c65d4985cf16655c04399bbff518232249
Ryan Malm <ryan.malm@us.af.mil>
2023-03-27 22:01:33 -0500

    game over ideas,  flyer boilerplate

--- js/actors/Flyer.js
+++ js/actors/Flyer.js
@@ -1,10 +1,10 @@
-class Hunter {
+class Hornet {
     constructor(x,y){
         this.x = x;
         this.y = y;
     }
     draw(){
-        
+        strokePolygon(this.x, this.y, 4, 3, ticker/10);
     }
     update(){
 
