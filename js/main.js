3a29e744e235f14ffb0eba7c09ef79c3336c05aa
Christer Kaitila <breakdance@mcfunkypants.com>
2023-06-05 13:10:38 -0700

    new room prefabs: t-intersection, platform

--- js/main.js
+++ js/main.js
@@ -277,6 +277,8 @@ function generateMap(config){
         tileMap.insertPrefab(rooms.stairsleft, Math.floor(mapRNG() * tileMap.widthInTiles), Math.floor(mapRNG() * tileMap.heightInTiles));
         tileMap.insertPrefab(rooms.stairsright, Math.floor(mapRNG() * tileMap.widthInTiles), Math.floor(mapRNG() * tileMap.heightInTiles));
         tileMap.insertPrefab(rooms.hut, Math.floor(mapRNG() * tileMap.widthInTiles), Math.floor(mapRNG() * tileMap.heightInTiles));
+        tileMap.insertPrefab(rooms.t_intersection, Math.floor(mapRNG() * tileMap.widthInTiles), Math.floor(mapRNG() * tileMap.heightInTiles));
+        tileMap.insertPrefab(rooms.platform, Math.floor(mapRNG() * tileMap.widthInTiles), Math.floor(mapRNG() * tileMap.heightInTiles));
     }
 
     tileMap.insertPrefab(rooms["c-shelter"], 10, 16)
