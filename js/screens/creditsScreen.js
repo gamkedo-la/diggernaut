29294ea0d4bb67ed3a3fcd17b3815e066dd69360
Ryan Malm <ryan.malm@gmail.com>
2023-06-25 21:03:38 -0500

    credits screen placeholder in place,  credit screen input added to titleScreen

--- js/screens/creditsScreen.js
+++ js/screens/creditsScreen.js
@@ -9,21 +9,50 @@ const creditsScreen = {
             this.congratsText +
             `
 HomeTeam GameDev Presents:
-Diggernaught
+Diggernaughts
 
 a JS game led by Ryan Malm from
 April 2 thru July 2 2023
 
 Ryan Malm: Lead Programmer, Game Design, Art
+Name Credit
+Name Credit
+Name Credit
+Name Credit
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
+moar credits
 `
         this.congratsText = additionalText;
         clearScreen('black');
         let verticalSpacing = 4;
         let creditsLength = this.credits.split(/\r?\n/).length * (gameFont.characterHeight + verticalSpacing) + canvas.height;
-        gameFont.drawText(this.credits, { x: 10, y: canvas.height - (ticker / 2) % creditsLength }, 0, verticalSpacing);
-        gameFont.drawText(this.credits, { x: 10, y: creditsLength + (ticker / 2) % creditsLength }, 0, verticalSpacing);
-        fillRect(0, 165, canvas.width, 15, '#111');
-        tinyFont.drawText("Press Enter to return to Title", { x: 190, y: 170 }, 0, 0);
+        gameFont.drawText(this.credits, { x: 10, y: Math.floor( canvas.height - (ticker / 2) % creditsLength) }, 0, verticalSpacing);
+        gameFont.drawText(this.credits, { x: 10, y: Math.floor(creditsLength + (ticker / 2) % creditsLength) }, 0, verticalSpacing);
+        fillRect(0, 425, canvas.width, 15, '#111');
+        tinyFont.drawText("Press Enter to return to Title", { x: 600, y: 430 }, 0, 0);
     },
 
     update: function () {
